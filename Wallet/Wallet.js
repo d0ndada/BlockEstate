9a075031fa9ab89ccd6964d5e173cfa0dc524359
d0ndada <pintoavila09@gmail.com>
2023-08-15 16:25:28 +0200

    addind properties fucntionallity

--- Wallet/Wallet.js
+++ Wallet/Wallet.js
@@ -6,6 +6,7 @@ const Transaction = require("./Transaction");
 class Wallet {
   constructor() {
     this.balance = INITIAL_BALANCE;
+    this.properties = [];
 
     this.keyPair = ec.genKeyPair();
     this.publicKey = this.keyPair.getPublic().encode("hex");
@@ -15,11 +16,30 @@ class Wallet {
     return this.keyPair.sign(crypto(data));
   }
 
+  ownsProperty(propertyId) {
+    return this.properties.includes(propertyId);
+  }
+  addProperty(propertyId) {
+    this.properties.push(propertyId);
+  }
+  removeProperty(propertyId) {
+    this.properties = this.properties.filter((id) => id !== propertyId);
+  }
+
   createTransaction({ amount, recipient }) {
     if (amount > this.balance) {
       throw new Error("Not enough funds");
     }
-    return new Transaction({ sender: this, recipient, amount });
+    if (propertyId && !this.ownsProperty(propertyId)) {
+      throw new Error("Thsi property don't belong to the wallet");
+    }
+    const transaction = new Transaction({ sender: this, recipient, amount });
+
+    if (propertyId) {
+      transaction.outputMap[recipient].propertyId = propertyId;
+      this.removeProperty(propertyId);
+    }
+    return transaction;
   }
 }
 
