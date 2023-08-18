9d1dbbf4eb1371130819b365f12c2e3fc1b425e4
d0ndada <pintoavila09@gmail.com>
2023-08-18 15:14:41 +0200

    update uuid

--- utilities/catchErrorAsync.js
+++
@@ -1,7 +0,0 @@
-module.exports = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch((error) => {
-      res.status(error.statusCode || 500).json({ error: error.message });
-    });
-  };
-};
