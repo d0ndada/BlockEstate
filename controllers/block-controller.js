af380a5a34ac56f2446c79924a582862e9a33ad1
d0ndada <pintoavila09@gmail.com>
2023-08-25 16:25:58 +0200

    adding block works

--- controllers/block-controller.js
+++ controllers/block-controller.js
@@ -1,5 +1,23 @@
+const { blockEstate } = require("../utilities/config");
+
 exports.addBlock = (req, res) => {
-  res.status(200).json({ success: true, data: "added block" });
+  const block = req.body.block;
+  const lastBlock = blockEstate.getLastBlock();
+  const hashIsCorrect = lastBlock.hash === block.previousHash;
+  const hasCorrectIndex = lastBlock.index + 1 === block.index;
+  if (hasCorrectIndex && hashIsCorrect) {
+    blockEstate.chain.push(block);
+    blockEstate.pendingList = [];
+    res.status(200).json({ success: true, data: block });
+  } else {
+    res
+      .status(400)
+      .json({
+        success: false,
+        errorMessage: "Block is not valid",
+        data: block,
+      });
+  }
 };
 
 exports.getLastBlock = (req, res) => {
