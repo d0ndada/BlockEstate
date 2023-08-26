b3c641ee90e39a682efee8eebe2672f15bedc855
d0ndada <pintoavila09@gmail.com>
2023-08-26 13:34:28 +0200

    accept bid working

--- controllers/transaction-controller.js
+++ controllers/transaction-controller.js
@@ -21,6 +21,14 @@ exports.createBid = (req, res) => {
 };
 
 exports.acceptBidTransaction = (req, res) => {
+  const acceptBid = blockEstate.acceptBidTransaction(
+    req.body.transactionId,
+    req.body.propertyId,
+    req.body.bidder,
+    req.body.price,
+    req.body.seller
+  );
+  blockEstate.addTransactionToPendingList(acceptBid);
   res.status(200).json({ success: true, data: "accepted bid of house" });
 };
 
