b22adbf98ce1f150a541db2aefbd572dbc29f287
d0ndada <pintoavila09@gmail.com>
2023-09-03 17:23:48 +0200

    chaning concesus regualr looop

--- controllers/consensus-controller.js
+++ controllers/consensus-controller.js
@@ -1,42 +1,47 @@
 const { blockEstate } = require("../utilities/config");
+const axios = require("axios");
 
-exports.consensus = (req, res) => {
+exports.consensus = async (req, res) => {
   const currentChainLength = blockEstate.chain.length;
   let maxLength = currentChainLength;
   let longestChain = null;
   let pendingList = null;
 
-  blockEstate.networkNodes.forEach((node) => {
+  for (let i = 0; i < blockEstate.networkNodes.length; i++) {
+    const node = blockEstate.networkNodes[i];
     console.log("Node: ", node);
 
-    axios(`${node}/api/blockchain`).then((data) => {
-      console.log("Data ifrån axios: ", data);
+    try {
+      const response = await axios.get(`${node}/api/blockchain`);
+      console.log("Data from axios: ", response.data);
 
-      if (data.data.chain.length > maxLength) {
-        maxLength = data.data.chain.length;
-        longestChain = data.data.chain;
-        pendingList = data.data.pendingList;
+      if (response.data.chain.length > maxLength) {
+        maxLength = response.data.chain.length;
+        longestChain = response.data.chain;
+        pendingList = response.data.pendingList;
       }
+    } catch (error) {
+      console.error("Error fetching data from node:", error);
+    }
+  }
 
-      if (!longestChain) {
-        return res.status(200).json({
-          success: true,
-          message: "Current chain is the longest.",
-        });
-      } else if (longestChain && !blockEstate.validateChain(longestChain)) {
-        return res.status(200).json({
-          success: true,
-          message: "Found a longer chain, but it's not valid.",
-        });
-      } else {
-        blockEstate.chain = longestChain;
-        blockEstate.pendingList = pendingList;
-        res.status(200).json({
-          success: true,
-          message: "Chain replaced with the longest valid chain.",
-          data: blockEstate,
-        });
-      }
+  if (!longestChain) {
+    return res.status(200).json({
+      success: true,
+      message: "Current chain is the longest.",
     });
-  });
+  } else if (longestChain && !blockEstate.validateChain(longestChain)) {
+    return res.status(200).json({
+      success: true,
+      message: "Found a longer chain, but it's not valid.",
+    });
+  } else {
+    blockEstate.chain = longestChain;
+    blockEstate.pendingList = pendingList;
+    return res.status(200).json({
+      success: true,
+      message: "Chain replaced with the longest valid chain.",
+      data: blockEstate,
+    });
+  }
 };
