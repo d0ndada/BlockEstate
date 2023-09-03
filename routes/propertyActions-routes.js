531b5976dfda77e2df7751e4718b83287b4e237a
d0ndada <pintoavila09@gmail.com>
2023-09-03 17:51:49 +0200

    update endpoint

--- routes/propertyActions-routes.js
+++ routes/propertyActions-routes.js
@@ -12,7 +12,6 @@ router.route("/list").post(listProperty);
 router.route("/bid").post(createBid);
 router.route("/acceptBid").post(acceptBid);
 router.route("/relist").post(relistProperty);
-// add delete have it in testing but not an endpoint************
 router.route("/deleteListing").post(deleteListing);
 
 module.exports = router;
